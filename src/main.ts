43ae4d66c314b4758652ab1a7fc67054656c17ee
BuiKiet <roiyano1utl@gmail.com>
2024-05-15 16:28:19 +0700

    init

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+declare const module: any;
+
+async function bootstrap() {
+  const port = process.env.PORT || 8080;
+  const app = await NestFactory.create(AppModule);
+  await app.listen(port, () => {
+    console.log(`Backend listenning http://127.0.0.1:${port}`);
+  });
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
+}
+bootstrap();
